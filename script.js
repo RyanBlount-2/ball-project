0b8d271952822d6fb9bbb0f1e460de7e3a8bd7c0
Ryan Blount <89652477+RyanBlount-2@users.noreply.github.com>
2022-04-19 16:34:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
 // Define parameters.
-var Time = 100;
-var Ball = document.getElementById('Ball');
-var Velocity_X = 10;
-var Velocity_Y = 10;
-var Position_X = 0;
-var Position_Y = 0;
-var Min_X = 0;
-var Min_Y = 0;
-var Max_X = 500;
-var Max_Y = 450;
+var time = 100;
+var ball = document.getElementById('ball');
+var velocityX = 10;
+var velocityY = 10;
+var positionX = 0;
+var positionY = 0;
+var minX = 0;
+var minY = 0;
+var maxX = 500;
+var maxY = 450;
 
 // Change the ball color upon clicking.
-function Random_Color() {
+function randomColor() {
    var r = ('0' + (Math.random() * 256 | 0).toString(16)).slice(-2);
    var g = ('0' + (Math.random() * 256 | 0).toString(16)).slice(-2);
    var b = ('0' + (Math.random() * 256 | 0).toString(16)).slice(-2);
    return '#' + r + g + b;
 }
-function New_Color() {
-   Ball.style.background = Random_Color();
+function newColor() {
+   ball.style.background = randomColor();
 }
-Ball.onclick = New_Color;
+ball.onclick = newColor;
 
 // Style the cursor.
 document.body.style.cursor = "crosshair";
 
 // Define how to move the ball in the x & y axes.
-function Move_Ball_X() {
-   var Ball = document.getElementById('Ball');
-   Position_X = Position_X + Velocity_X;
-   Ball.style.left = Position_X + 'px';
+function moveBallX() {
+   var ball = document.getElementById('ball');
+   positionX = positionX + velocityX;
+   ball.style.left = positionX + 'px';
 }
-function Move_Ball_Y() {
-   var Ball = document.getElementById('Ball');
-   Position_Y = Position_Y + Velocity_Y;
-   Ball.style.top = Position_Y + 'px';
+function moveBallY() {
+   var ball = document.getElementById('ball');
+   positionY = positionY + velocityY;
+   ball.style.top = positionY + 'px';
 }
 
 // Determine if within range and if velocity needs to be changed.
-function Edge_Detection_X() {
-   if (Position_X >= Min_X && Position_X <= Max_X) {
-      Move_Ball_X();
+function edgeDetectionX() {
+   if (positionX >= minX && positionX <= maxX) {
+      moveBallX();
    }
-   else { Velocity_X = -Velocity_X;
-      Move_Ball_X(); }
+   else { velocityX = -velocityX;
+      moveBallX(); }
 }
-function Edge_Detection_Y() {
-   if (Position_Y >= Min_Y && Position_Y <= Max_Y) {
-      Move_Ball_Y();
+function edgeDetectionY() {
+   if (positionY >= minY && positionY <= maxY) {
+      moveBallY();
    }
    else {
-      Velocity_Y = -Velocity_Y;
-      Move_Ball_Y();
+      velocityY = -velocityY;
+      moveBallY();
    }
 }
 
 // Main function.
-function Main_Function() {
-   Edge_Detection_X();
-   Edge_Detection_Y();
+function mainFunction() {
+   edgeDetectionX();
+   edgeDetectionY();
 }
 
 // Move ball at a set interval.
-setInterval(Main_Function, Time);
+setInterval(mainFunction, time);
